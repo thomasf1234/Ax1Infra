53267388c45afb3d73747a1ef8735178851d28ad
Thomas F <thomas.fisher@footballpools.com>
2017-06-15 07:17:57 +0100

    [#0] TF : reinstating ax1package script

--- puppetlabs/code/modules/base/manifests/packages.pp
+++ puppetlabs/code/modules/base/manifests/packages.pp
@@ -1,6 +1,15 @@
 class base::packages(
   $packages = [ "htop","ncdu", "vim", "curl"]
 ) {
+  file { "ax1package utility script":
+    path => "/usr/local/bin/ax1package",
+    owner => "root",
+    group => "root",
+    mode => "0755",
+    replace => true,
+    source => 'puppet:///modules/base/usr/local/bin/ax1package'
+  }
+
   package { $packages:
     ensure => latest
   }
